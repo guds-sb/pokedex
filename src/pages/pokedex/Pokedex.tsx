1ed72056f555b8fa222011c5a61067916a519b21
Guds <gustavoberw@gmail.com>
2021-09-04 13:59:34 -0300

    added api call

--- src/pages/pokedex/Pokedex.tsx
+++ src/pages/pokedex/Pokedex.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getAllPokemon } from '../../services/pokedex/PokedexService';
 
 type PokedexProps = {
-  test?: string
 };
 
 const Pokedex = (props: PokedexProps) => {
+  const [serviceError, setServiceError] = useState<any>(null!);
+  const [serviceResponse, setServiceResponse] = useState<any>({});
+
+  useEffect(() => {
+    getAllPokemon().subscribe({
+      next: (data: any) => setServiceResponse(data),
+      error: (error: any) => setServiceError(error)
+    });
+  }, []);
+
+  console.log(serviceError);
+  console.log(serviceResponse);
+
+  if (serviceError) {
+    return (
+      <div>
+        {serviceError.message}
+      </div>
+    );
+  }
+
   return (
     <div>
-      {props.test}
+      {serviceResponse?.data?.results?.map((poke: any, index: number) => <p key={index}>{poke.name}</p>)}
     </div>
   );
 }
 
-export default Pokedex
+export default Pokedex;
--- src/services/pokedex/PokedexService.tsx
+++ src/services/pokedex/PokedexService.tsx
@@ -1,1 +1,7 @@
-export {}
+import { from } from 'rxjs';
+import axios from 'axios';
+
+export const getAllPokemon = () => {
+  const requestPromise = axios.get(`${process.env.REACT_APP_POKEDEX_API}/pokemdon?limit=898`);
+  return from(requestPromise);
+}
